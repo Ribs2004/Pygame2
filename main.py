63b273f1a6e0f21ab087e18b5e788dd41af7fcd3
Ribs2004 <pedrocrn@al.insper.edu.br>
2022-11-23 11:02:12 -0300

    movimento carro

--- main.py
+++ main.py
@@ -7,55 +7,77 @@ pygame.init()
 # ----- Variáveis
 LENGTH = 840
 WIDTH = 650
-CAR_LENGTH = 84
-CAR_WIDTH = 65
+CAR_LENGTH = 140
+CAR_WIDTH = 105
 # ----- Gera tela principal
 window = pygame.display.set_mode((840, 650))
 pygame.display.set_caption('Need For Speed 2D')
 
-# ----- Inicia Assets
-def load_assets():
-    assets = {}
-    assets['background'] = pygame.image.load('img/background.png').convert()
-    assets['imagem_carro'] = pygame.image.load('img/carro.png').convert_alpha()
-    assets['imagem_carro_transform'] = pygame.transform.scale(assets['imagem_carro'],(CAR_LENGTH,CAR_WIDTH))
-    return assets
+# ----- Inicia Assets 
+background = pygame.image.load('img/background.png').convert()
+img = pygame.image.load('img/carro.png').convert_alpha()
+img_transform = pygame.transform.scale(img ,(CAR_LENGTH,CAR_WIDTH))
+
 # ----- Inicia estruturas de dados
 game = True
+
+# ----- Variável para ajuste de velocidade
+clock = pygame.time.Clock()
+FPS = 30
 # ----- Definindo os novos tipos
 class Car(pygame.sprite.Sprite):
-    def __init__(self,groups,assets):
+    def __init__(self,img):
         pygame.sprite.Sprite.__init__(self)
-        self.image = assets['imagem_carro']
-        self.mask = pygame.mask.from_surface(self.image)
+
+        self.image = img
         self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH / 2
-        self.rect.bottom = LENGTH - 10
+        self.rect.x = 378
+        self.rect.y = 500
         self.speedx = 0
-        self.groups = groups
-        self.assets = assets
 
     def update(self):
             self.rect.x += self.speedx
-            if self.rect.right > WIDTH:
-                self.rect.right = WIDTH
-            if self.rect.left < 0:
-                self.rect.left = 0
-assets = load_assets()
+
+            # Mantem dentro da tela
+            if self.rect.right > WIDTH+75:
+                self.rect.right = WIDTH+75
+            if self.rect.left < 120:
+                self.rect.left = 120
+
+#assets = load_assets()
+all_sprites = pygame.sprite.Group()
+carro = Car(img_transform)
+all_sprites.add(carro)
+
 # ===== Loop principal =====
 while game:
+    clock.tick(FPS)
+
     # ----- Trata eventos
     for event in pygame.event.get():
         # ----- Verifica consequências
         if event.type == pygame.QUIT:
             game = False
+        if event.type == pygame.KEYDOWN:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                carro.speedx -= 8
+            if event.key == pygame.K_RIGHT:
+                carro.speedx += 8
+        # Verifica se soltou alguma tecla.
         if event.type == pygame.KEYUP:
-            game = False
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                carro.speedx += 8
+            if event.key == pygame.K_RIGHT:
+                carro.speedx -= 8
+        
 
+    all_sprites.update()
     # ----- Gera saídas
     window.fill((0, 0, 255))  # Preenche com a cor branca
-    window.blit(assets['background'], (0, 0))
-
+    window.blit(background, (0, 0))
+    all_sprites.draw(window)
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
 
